92c2544be230e33e0dc9da47f681956e39618c8d
sherbs <darthsherbs@gmail.com>
2017-10-04 19:08:36 -0500

    editing using curses library -- can't backspace

---
+++ papero2.py
@@ -0,0 +1,43 @@
+import curses
+
+def initProg():
+	stdscr = curses.initscr()
+	# clear screen
+	stdscr.clear()
+	curses.cbreak()
+	# don't write to window
+	curses.noecho()
+
+	modMode(stdscr)
+	editMode(stdscr)
+	teardown(stdscr)
+
+def modMode(windowObj):
+	windowObj.addstr(0, 0, "***Modification Mode***\n", curses.A_STANDOUT)
+
+def editMode(windowObj):
+	# write this to screen first
+	windowObj.addstr(0, 0, "***Edit Mode***\n", curses.A_STANDOUT)
+	windowObj.addstr("\n")
+	# gather text and print it until user presses ESC
+	c = windowObj.getch()
+	while(c != 27):
+		# if backspace is pressed, delete previous character
+		if(c == 8):
+			windowObj.delch(windowObj.getyx())
+			windowObj.refresh()
+		
+		windowObj.addch(c)
+		c = windowObj.getch()
+	
+
+def teardown(windowObj):
+	curses.nocbreak()
+	windowObj.keypad(False)
+	curses.echo()
+	# restore terminal to original state
+	curses.endwin()
+
+
+### MAIN ###
+initProg()
